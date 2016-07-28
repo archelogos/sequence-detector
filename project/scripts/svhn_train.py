f211ef4784301f0d533c335176dfb03cd4824d19
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-07-28 16:50:22 +0200

    Ready to GCP
    -> Stable scripts of SVHN sequences
    -> Load the preprocessed data from the pickle
    -> Build the CNN network
    -> Train a consistent and stable network
    -> Save the results
    -> First attempt in GCP: 100k, let’s see how much time it takes

--- project/scripts/svhn_train.py
+++ project/scripts/svhn_train.py
@@ -20,95 +20,96 @@ import gzip
 import os
 import re
 import sys
-import tarfile
-import h5py
-import glob
 
-import numpy as np
 import tensorflow as tf
-from scipy import ndimage
-from scipy.misc import imresize
-from six.moves.urllib.request import urlretrieve
-from six.moves import cPickle as pickle
-from PIL import Image
-from tensorflow.models.image.cifar10 import cifar10_input
+import svhn
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+tf.app.flags.DEFINE_string('train_dir', 'trained_models',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 20000,
+tf.app.flags.DEFINE_integer('max_steps', 101,
                             """Number of batches to run.""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False,
+                            """Whether to log device placement.""")
 
 
 def train():
-  with tf.Graph().as_default():
-    BATCH_SIZE = 64
-    PATCH_SIZE = 5
-    DEPTH_1 = 16
-    DEPTH_2 = 32
-    DEPTH_3 = 64
-    HIDDEN_NODES = 256
 
+  with tf.Graph().as_default():
     # Get data and labels
-    SVHN_data = svhn_input.SVHN_data
-    train_dataset = save['train_dataset']
-    train_labels = save['train_labels']
-    valid_dataset = save['valid_dataset']
-    valid_labels = save['valid_labels']
-    test_dataset = save['test_dataset']
-    test_labels = save['test_labels']
+    SVHN_data = svhn.get_data()
+    train_dataset = SVHN_data['train_dataset']
+    train_labels = SVHN_data['train_labels']
+    valid_dataset = SVHN_data['valid_dataset']
+    valid_labels = SVHN_data['valid_labels']
+    test_dataset = SVHN_data['test_dataset']
+    test_labels = SVHN_data['test_labels']
 
     # Input data.
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-    tf_train_labels = tf.placeholder(tf.int64, shape=(BATCH_SIZE, N+1))
-    #tf_valid_dataset = tf.constant(valid_dataset)
-    #tf_test_dataset = tf.constant(test_dataset)
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(FLAGS.BATCH_SIZE, FLAGS.IMAGE_SIZE, FLAGS.IMAGE_SIZE, FLAGS.NUM_CHANNELS))
+    tf_train_labels = tf.placeholder(tf.int64, shape=(FLAGS.BATCH_SIZE, FLAGS.N+1))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = svhn.inference(tf_train_dataset)
+    CNN = svhn.CNN()
+    logits = CNN.inference(tf_train_dataset)
 
     # Calculate loss.
-    loss = svhn.loss(logits, tf_train_labels)
+    loss = CNN.loss(logits, tf_train_labels)
 
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
-    svhn.train(loss)
+    optimizer = CNN.train(loss)
+
+    # Preds to estimate
+    train_prediction = CNN.predict(tf_train_dataset)
+    test_prediction = CNN.predict(tf_test_dataset)
+    valid_prediction = CNN.predict(tf_valid_dataset)
 
     # Create a saver.
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(tf.all_variables())
 
     # Build an initialization operation to run below.
     init = tf.initialize_all_variables()
 
     # Start running operations on the Graph.
-    session = tf.Session()
-    session.run(init)
+    sess = tf.Session(config=tf.ConfigProto(
+        log_device_placement=FLAGS.log_device_placement))
+    sess.run(init)
 
-    for step in range(NUM_STEPS):
-      offset = (step * BATCH_SIZE) % (train_labels.shape[0] - BATCH_SIZE)
-      batch_data = train_dataset[offset:(offset + BATCH_SIZE), :, :, :]
-      batch_labels = train_labels[offset:(offset + BATCH_SIZE),:]
+    #saver.restore(session, os.path.join(FLAGS.train_dir, 'model.ckpt'))
+    #print('Model Restored')
+    #print('Initialized')
+
+    for step in range(FLAGS.max_steps):
+      offset = (step * FLAGS.BATCH_SIZE) % (train_labels.shape[0] - FLAGS.BATCH_SIZE)
+      batch_data = train_dataset[offset:(offset + FLAGS.BATCH_SIZE), :, :, :]
+      batch_labels = train_labels[offset:(offset + FLAGS.BATCH_SIZE),:]
       feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
-      _, l, predictions = session.run(
+      _, l, predictions = sess.run(
         [optimizer, loss, train_prediction], feed_dict=feed_dict)
-      if (step % 10000 == 0):
+      if (step % 100 == 0):
         print('Minibatch loss at step %d: %f' % (step, l))
-        print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-        print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
+        #print('Minibatch accuracy: %.1f%%' % svhn.accuracy(predictions, batch_labels))
+        #print('Validation accuracy: %.1f%%' % svhn.accuracy(valid_prediction.eval(session=sess), valid_labels))
 
-    sp = saver.save(session, "../tmp/test.ckpt")
+    print('Out of training')
+    checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+    sr = saver.save(sess, checkpoint_path)
     print('Model Saved')
-    print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+    #print('Evaluating the final performance')
+    #print('Test accuracy: %.1f%%' % svhn.accuracy(test_prediction.eval(session=sess), test_labels))
 
 
 def main(argv=None):  # pylint: disable=unused-argument
   svhn.load_data()
-  #if tf.gfile.Exists(FLAGS.train_dir):
-  #  tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  #tf.gfile.MakeDirs(FLAGS.train_dir)
+  if tf.gfile.Exists(FLAGS.train_dir):
+    tf.gfile.DeleteRecursively(FLAGS.train_dir)
+  tf.gfile.MakeDirs(FLAGS.train_dir, 0777) # force permissions
   train()
 
 if __name__ == '__main__':
